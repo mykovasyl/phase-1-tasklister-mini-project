9a890ca0bbee830c6fe7a1c94dabb2cb3d4e7d12
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-02-15 21:01:58 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    editMyTodos(e.target['new-task-description'].value, e.target.priorities.value);
+    form.reset();
+  });
 });
+
+function editMyTodos(task, priority) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteTask);
+  btn.textContent = 'X';
+  p.textContent = `${task} `;
+  p.style.color = `${priority}`;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
